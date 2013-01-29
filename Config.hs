5b7c81e30848b85da1d074e72155e32ba25be23c
Richard Pilz <richard.pilz.online@gmx.de>
2013-01-29 16:10:01 +0100

    Files can be added to and removed from Config.

--- Config.hs
+++ Config.hs
@@ -1,6 +1,7 @@
 module Config where
 
 import TagFS (TagSet)
+import qualified TagSet as T
 import qualified Data.Map as M
 import Data.Functor
 import Safe
@@ -16,3 +17,9 @@ readConfig f = readMay <$> readFile f
 
 writeConfig :: FilePath -> Config -> IO ()
 writeConfig f c = writeFile f $ show c
+
+addFile :: (FilePath, FilePath) -> Config -> Config
+addFile (n,p) c = Config (T.addFile n $ tagSet c) (M.insert n p $ mapping c)
+
+removeFile :: FilePath -> Config -> Config
+removeFile n c = Config (T.removeFile n $ tagSet c) (M.delete n $ mapping c)
