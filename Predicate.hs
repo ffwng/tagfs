eba4eb0544c35f85cf6aa5722920839dcce20343
Richard Pilz <richard.pilz.online@gmx.de>
2013-02-15 21:03:23 +0100

    Adds predicate parsing and handling code.

---
+++ Predicate.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE DeriveFunctor #-}
+module Predicate (
+	Condition(..), ECondition(..),
+	Tree,
+	parse, interpret, eval,
+	evalString
+) where
+
+import Data.Functor
+import Control.Monad.Free
+
+import Predicate.Parser (Var, Val, Op, Expr)
+import qualified Predicate.Parser as P
+
+data OpBranch a = And a a | Or a a | Not a
+	deriving Functor
+
+type Tree = Free OpBranch
+
+data Condition = Exists Var | Is Op Var Val
+
+data ECondition = Cond Condition | Const Bool
+
+parse :: String -> Maybe (Tree ECondition)
+parse s = convert <$> P.parseString s
+
+convert :: Expr -> Tree ECondition
+convert (P.Const b) = Pure (Const b)
+convert (P.Leaf v) = Pure (Cond (Exists v))
+convert (P.LeafOp o v1 v2) = Pure (Cond (Is o v1 v2))
+convert (P.Not e) = Free (Not (convert e))
+convert (P.Branch P.And a b) = Free (And (convert a) (convert b))
+convert (P.Branch P.Or a b) = Free (Or (convert a) (convert b))
+
+interpret :: (Condition -> Bool) -> Tree ECondition -> Tree Bool
+interpret f = fmap (\c -> case c of
+	Cond c' ->  f c'
+	Const b -> b )
+
+eval :: Tree Bool -> Bool
+eval (Pure b) = b
+eval (Free (And a b)) = eval a && eval b
+eval (Free (Or a b)) = eval a || eval b
+eval (Free (Not a)) = not $ eval a
+
+evalString :: (Condition -> Bool) -> String -> Maybe Bool
+evalString f s = eval . interpret f <$> parse s
---
+++ Predicate/Parser.hs
@@ -0,0 +1,96 @@
+module Predicate.Parser (
+	Expr(..), BOp(..), Op(..), Var(..), Val(..),
+	expression, parseString
+) where
+
+import Control.Monad.Identity
+import Control.Applicative ((<$>), (<*))
+import Text.Parsec
+import Text.Parsec.String
+import Text.Parsec.Expr
+import Text.Parsec.Language
+import qualified Text.Parsec.Token as Token
+
+data Expr = Const Bool
+			| Leaf Var
+			| LeafOp Op Var Val
+			| Not Expr
+			| Branch BOp Expr Expr
+			deriving (Show)
+
+data BOp = And | Or deriving (Show)
+
+data Op = Equals | Greater | Less deriving (Show)
+
+type Var = String
+
+data Val = StringVal String | IntVal Integer
+			deriving (Show)
+
+languageDef :: LanguageDef st
+languageDef = emptyDef
+	{ Token.commentStart    = "/*"
+	, Token.commentEnd	   = "*/"
+	, Token.commentLine	   = "//"
+	, Token.identStart	   = letter
+	, Token.identLetter	   = alphaNum
+	, Token.reservedNames   = ["true", "false"]
+	, Token.reservedOpNames = [ ":", "=", "<", ">", "&&", "||", "!"]
+	}
+
+lexer :: Token.TokenParser st
+lexer = Token.makeTokenParser languageDef
+
+identifier :: Parser String
+identifier = Token.identifier lexer <|> Token.stringLiteral lexer
+
+reserved :: String -> Parser ()
+reserved = Token.reserved lexer
+
+reservedOp :: String -> Parser ()
+reservedOp = Token.reservedOp lexer
+
+parens :: Parser a -> Parser a
+parens = Token.parens lexer
+
+integer :: Parser Integer
+integer = Token.integer lexer
+
+expression :: Parser Expr
+expression = buildExpressionParser operators term <* eof
+
+operators :: OperatorTable String () Identity Expr
+operators = [ [Prefix (reservedOp "!" >> return Not)]
+			, [Infix (reservedOp "&&" >> return (Branch And)) AssocLeft]
+			, [Infix (reservedOp "||"  >> return (Branch Or)) AssocLeft]
+			]
+
+term :: Parser Expr
+term = parens expression
+	<|> (reserved "true" >> return (Const True))
+	<|> (reserved "false" >> return (Const False))
+	<|> leaf
+
+value :: Parser Val
+value = StringVal <$> identifier
+	  <|> IntVal <$> integer
+
+leaf :: Parser Expr
+leaf = do
+	a1 <- identifier
+	rel a1 <|> return (Leaf a1)
+	where rel a1 = do
+		op <- relation
+		a2 <- value
+		return $ LeafOp op a1 a2
+
+relation :: Parser Op
+relation = (reservedOp ">" >> return Greater)
+	<|> (reservedOp "<" >> return Less)
+	<|> (reservedOp ":" >> return Equals)
+	<|> (reservedOp "=" >> return Equals)
+
+parseString :: String -> Maybe Expr
+parseString str = case parse expression "" str of
+	Left e -> Nothing
+	Right r -> Just r
